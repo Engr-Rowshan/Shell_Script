ba09ebf292a1b680e42bfbc8191f0df4023ef1a2
Engr. Syed Rowshan Ali <engr.rowshan@gmail.com>
2020-04-24 03:41:56 +0600

    variable and condition added

--- backup.sh
+++ backup.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+if [ -z $1]; then
+	echo "Please metion a directory or file to backuo"
+	echo "Backup not succesful"
+	exit 1
+else
+	input=$1
+fi
+
+if [ -z $2 ]; then
+	echo "Destination file not set."
+	echo "Mention destion as 2nd parameter"
+	exit 2
+else
+	output=$2
+fi
+
 #tar -czf /tmp/myhomebak.tar.gz /home/rowshan 2> /dev/null
 stat_time=$(date)
 
@@ -8,7 +24,7 @@ clear
 
 echo "Backup Starting at: ${stat_time}/n"
 printf "\n"
-tar cf - /home/rowshan -P 2> /dev/null | pv -s $(du -sb /home/rowshan 2> /dev/null | awk '{print $1}')  | gzip > /tmp/rowshan.bak.tar.gz
+tar cf - $input -P 2> /dev/null | pv -s $(du -sb $input 2> /dev/null | awk '{print $1}')  | gzip > $output
 end_time=$(date)
 printf "\n"
 echo "Backup End at: ${end_time}"
